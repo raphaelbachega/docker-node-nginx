63850e3b2395d51f873086d974f0e7fcbb59180d
raphaelbachega <bachegamessias@gmail.com>
2023-07-21 16:46:18 -0300

    first commit

---
+++ node/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sqlInsert = `INSERT INTO people (name) VALUES ('luke'),('Nelson'),('Pinguin');`
+connection.query(sqlInsert)
+
+var resultList
+const sqlSelect = `SELECT * FROM people`
+
+connection.query(sqlSelect, (err, result, fields)=> {
+    if (err) {
+        throw err;
+    }
+    resultList = result.map( person => JSON.stringify(person.name));
+})
+
+connection.end()
+
+app.get('/', (req,res) => {
+    res.send(`<h1>FullCycle</h1>\n` +
+             `${resultList}`)
+})
+
+app.listen(port, ()=> {
+    console.log('rodando na porta '+ port)
+})
